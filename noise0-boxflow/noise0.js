4e23660b98860d74b93ca7c3891bd5c2184a4243
jli <jli@circularly.org>
2019-09-22 20:57:03 -0400

    up

--- noise0-boxflow/noise0.js
+++ noise0-boxflow/noise0.js
@@ -1,7 +1,18 @@
-let TARGET_NUM_BOXES = 10000;
+// TODO:
+// - tweak stroke drawing
+// - try deviations from exact direction (gaussian, private noise?), or more momentum?
+// - make new boxes smaller.
+// - make old boxes shrink to 0 instead of just removing?
+// - use gui library
+
+
+let TARGET_NUM_BOXES = 5000;
 let BOX_SIZE;
 let NUM_ROWS, NUM_COLS;
 let OPACITY = 0.1;
+let STROKE_OPACITY = 0.3;
+let DRAW_STROKE = true;
+let SPEED_MULT = 1.0;
 
 let ACC_LIMIT = 2.5;
 let VEL_LIMIT = 4.0;
@@ -10,7 +21,7 @@ let FLOW_NOISE_TIME_MULT = 0.003;
 let SIZE_NOISE_MULT = 0.03;
 
 let NUM_REFRESHED_THINGS_PER_FRAME = 5;
-let RESET_EVERY_FRAMECOUNT = 1000;
+let RESET_EVERY_FRAMECOUNT = 500;
 let THINGS = [];
 
 function wrap(x, mx) {
@@ -33,6 +44,11 @@ class Thing {
 
   draw(col) {
     fill(col, OPACITY);
+    if (DRAW_STROKE) {
+      let stroke_bright = col > 50 ? 0 : 40;
+      stroke(stroke_bright, STROKE_OPACITY);
+      // stroke(col, 0.2);
+    }
     rect(this.pos.x, this.pos.y,
       BOX_SIZE * this.size_mult, BOX_SIZE * this.size_mult);
   }
@@ -42,7 +58,7 @@ class Thing {
       this.size_mult * map(
           noise(this.pos.x * SIZE_NOISE_MULT, this.pos.y * SIZE_NOISE_MULT),
           0, 1, 0.8, 1.2),
-      0.5, 2.5);
+      0.4, 2.5);
     let flow = createVector(
       map(noise(this.pos.x * FLOW_NOISE_POS_MULT,
                 this.pos.y * FLOW_NOISE_POS_MULT,
@@ -54,7 +70,7 @@ class Thing {
           0, 1, -.3, .3));
     this.acc.add(flow).limit(ACC_LIMIT);
     this.vel.add(this.acc).limit(VEL_LIMIT);
-    this.pos.add(this.vel);
+    this.pos.add(this.vel.mult(SPEED_MULT));
     this.pos.x = wrap_x(this.pos.x);
     this.pos.y = wrap_y(this.pos.y);
   }
@@ -72,14 +88,14 @@ function setup() {
   colorMode(HSB);
   noStroke();
   createCanvas(windowWidth, windowHeight);
-  setup_geometry();
-  init_things();
+  init_world();
   background(0);
 }
 
 function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
-function init_things() {
+function init_world() {
+  setup_geometry();  // in case window size changed
   THINGS = [];
   let i = 0;
   for (let r = 0; r < NUM_ROWS; ++r) {
@@ -122,7 +138,20 @@ function draw() {
   if (count >= RESET_EVERY_FRAMECOUNT) {
     count = 0;
     dir *= -1;
-    init_things();
+    init_world();
   }
   // noLoop();
+}
+
+function keyPressed() {
+  switch (key) {
+    case 'p': toggle_paused(); break;
+    case 'r': init_world(); break;
+  }
+}
+
+let PAUSED = false;
+function toggle_paused() {
+  PAUSED = !PAUSED;
+  if (PAUSED) { noLoop(); } else { loop(); }
 }
